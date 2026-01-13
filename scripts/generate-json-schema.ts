94127cbb85af29884e3e24e2a1ca54fa4cdec6bb
Charlie Harding <charlie_harding@icloud.com>
2026-01-13 23:55:37 +0100

    Move schemas to outside relevant folder

--- scripts/generate-json-schema.ts
+++ scripts/generate-json-schema.ts
@@ -3,14 +3,14 @@ import * as z from 'zod';
 import { Network, NetworkDisplay } from '../src/model.ts';
 
 await fs.writeFile(
-  'public/networks/schema.json',
+  'public/networks-schema.json',
 
   JSON.stringify(z.toJSONSchema(Network, { io: 'input', target: 'openapi-3.0' }), null, 2),
   'utf-8',
 );
 
 await fs.writeFile(
-  'public/maps/schema.json',
+  'public/maps-schema.json',
   JSON.stringify(z.toJSONSchema(NetworkDisplay, { io: 'input', target: 'openapi-3.0' }), null, 2),
   'utf-8',
 );
